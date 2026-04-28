8d7700908780d89a3238632fd67caae01d3d78f6
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-04-28 19:57:46 +0530

    Day 2 - Mongo Db Connection with token file

--- LinkedIn/Backend/controllers/authController.js
+++ LinkedIn/Backend/controllers/authController.js
@@ -1,14 +1,21 @@
 import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
+import genToken from "../config/token.js";
 
 export const signup = async (req, res) => {
   try {
-    let { firstName, userName, email, password } = req.body;
+    let { firstName, lastName, userName, email, password } = req.body;
 
     let existEmail = await User.findOne({ email });
 
     if (existEmail) {
       return res.status(400).json({ message: "User Email  already exists !" });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password Must be Atleast 8 Chararcters" });
     }
 
     let existUserName = await User.findOne({ userName });
@@ -26,8 +33,17 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
 
+    let token = await genToken(user_.id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      secure: process.env.NODE_ENVIRONMENT === "production",
+    });
+
     res.status(201).json(user);
   } catch (error) {
-    return res.status(500).json({ message: "Server error" });
+    return res.status(500).json({ message: "Sign up Error" });
+    console.log(error);
   }
 };
--- LinkedIn/Backend/index.js
+++ LinkedIn/Backend/index.js
@@ -2,10 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 let app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
 let port = process.env.PORT || 5000;
 app.use("/api/auth", authRouter);
 
