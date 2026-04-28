b9e21727d2c08924e0917fd389cf751cfbb83ed3
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-04-28 21:59:14 +0530

    Day 2 - login and logout functionality done

--- LinkedIn/Backend/controllers/authController.js
+++ LinkedIn/Backend/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
 import genToken from "../config/token.js";
 
+//register
 export const signup = async (req, res) => {
   try {
     let { firstName, lastName, userName, email, password } = req.body;
@@ -29,11 +30,12 @@ export const signup = async (req, res) => {
     const user = await User.create({
       firstName,
       lastName,
+      userName,
       email,
       password: hashedPassword,
     });
 
-    let token = await genToken(user_.id);
+    let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -44,6 +46,51 @@ export const signup = async (req, res) => {
     res.status(201).json(user);
   } catch (error) {
     return res.status(500).json({ message: "Sign up Error" });
+    console.log(error);
+  }
+};
+
+//login
+export const login = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User Does not   already exists !" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    let token = await genToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      secure: process.env.NODE_ENVIRONMENT === "production",
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: "Log in Error" });
+    console.log(error);
+  }
+};
+
+//logout
+export const logOut = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({ message: "Logout Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Log out Error" });
     console.log(error);
   }
 };
--- LinkedIn/Backend/routes/authRoutes.js
+++ LinkedIn/Backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/authController.js";
+import { login, logOut, signup } from "../controllers/authController.js";
 
 let authRouter = express.Router();
 
 authRouter.post("/signup", signup);
+authRouter.post("/login", login);
+authRouter.post("/logout", logOut);
 
 export default authRouter;
